0e8f7cc642c7177e453ccc4d0a92632ec181072d
andydangit <andy.dang.it@gmail.com>
2024-07-20 14:17:50 -0500

    Initial Commit

---
+++ SwiftUi-Weather/SwiftUi_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_WeatherApp.swift
+//  SwiftUi-Weather
+//
+//  Created by FartFire on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
