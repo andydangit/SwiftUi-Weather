46eafc4c4cd8c8c6bfb07e14d4162cfe45d7b958
andydangit <andy.dang.it@gmail.com>
2024-07-20 14:50:08 -0500

    Add - background color and fill edge

--- SwiftUi-Weather/ContentView.swift
+++ SwiftUi-Weather/ContentView.swift
@@ -9,14 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("First Git Commit")
-            Text("in SwiftUi")
+        ZStack{
+            Color(.blue)
+                .edgesIgnoringSafeArea( .all)
         }
-        .padding()
     }
 }
 
