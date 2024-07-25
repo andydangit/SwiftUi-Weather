12e54b8662317b7c26a6d8ae167a393a620d8000
andydangit <andy.dang.it@gmail.com>
2024-07-24 20:49:21 -0500

    Refactor: Move Weather Button into its own file

--- SwiftUi-Weather/ContentView.swift
+++ SwiftUi-Weather/ContentView.swift
@@ -116,18 +116,4 @@ struct MainWeatherView: View {
     }
 }
 
-struct WeatherButton: View {
-    
-    var title: String
-    var textColor: Color
-    var backgroundColor: Color
-    
-    var body: some View {
-        Text(title)
-            .frame(width: 280, height: 50)
-            .background(backgroundColor)
-            .foregroundColor(textColor)
-            .font(.system(size: 20, weight: .bold, design: .default))
-            .cornerRadius(10)
-    }
-}
+
