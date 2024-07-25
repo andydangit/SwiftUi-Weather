736080ebb2d100ab355d9fcd667d0a732c060242
andydangit <andy.dang.it@gmail.com>
2024-07-24 21:14:57 -0500

    Refactor: Add @Binding and remove logic to BackgroundView

--- SwiftUi-Weather/ContentView.swift
+++ SwiftUi-Weather/ContentView.swift
@@ -14,8 +14,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            BackgroundView(topColor: isNight ? .black : .blue,
-                           bottomColor: isNight ? .gray : Color("lightBlue"))
+            BackgroundView(isNight: $isNight)
             VStack {
                 CityTextView(cityName: "Oklahoma City, OK")
                 
@@ -76,11 +75,11 @@ struct WeatherDayView: View {
 
 struct BackgroundView: View {
     
-    var topColor: Color
-    var bottomColor: Color
+    @Binding var isNight: Bool
     
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, 
+                                                   isNight ? .gray : Color("lightBlue")]),
                        startPoint: .topLeading,
                        endPoint: .bottomTrailing)
         .edgesIgnoringSafeArea( .all)
